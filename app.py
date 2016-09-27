31b901ec8d07c0df1886f8c0ee6ef4e43af1a8e3
Mingxiang Zhu <zmxrice@qq.com>
2016-09-27 11:16:18 -0400

    adding export graph module

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
-from flask import Flask, send_file, json, request, redirect, url_for
+import time
+from flask import Flask, send_file, json, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 import networkx as nx
 from networkx.readwrite import json_graph
 
 UPLOAD_FOLDER = 'upload/'
+GENERATE_FOLDER = 'generate/'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['GENERATE_FOLDER'] = GENERATE_FOLDER
+app.config['GENERATED_GRAPH'] = None
 
 @app.route("/")
 def index():
@@ -18,8 +22,8 @@ def index():
 def generate_graph():
     try:
         action = request.json['userAction']
-        print action
         G = eval('nx.'+action)
+        app.config['GENERATED_GRAPH'] = G
         data = json_graph.node_link_data(G)
         return json.dumps(data)
     except:
@@ -29,18 +33,49 @@ def generate_graph():
 def upload_graph():
     if 'file' not in request.files:
         flash('No file part')
-        return redirect(request.url)
+        return "error"
     file = request.files['file']
 
     if file.filename == '':
         flash('No selected file')
-        return redirect(request.url)
+        return "error"
     if file:
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         gFile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         print gFile
         return "success"
+
+@app.route('/exportGraph', methods=['POST'])
+def export_graph():
+    if app.config['GENERATED_GRAPH']:
+        requestFormat = request.json['fmat']
+        filename = str(int(time.time()))
+        #choose graph generator based on user selection
+        if requestFormat == "adjlist":
+            filename += ".adjlist"
+            fPath = os.path.join(app.config['GENERATE_FOLDER'], filename)
+            fh=open(fPath,'wb')
+            nx.write_adjlist(app.config['GENERATED_GRAPH'], fh)
+            fh.close()
+
+        elif requestFormat == "edgelist":
+            filename += ".edgelist"
+            fPath = os.path.join(app.config['GENERATE_FOLDER'], filename)
+            fh=open(fPath,'wb')
+            nx.write_edgelist(app.config['GENERATED_GRAPH'], fh)
+            fh.close()
+
+        return url_for('generated_file',filename=filename)
+    else:
+        return "error"
+
+
+@app.route('/generated/<filename>')
+def generated_file(filename):
+    return send_from_directory(app.config['GENERATE_FOLDER'],filename, as_attachment=True)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- static/js/app.js
+++ static/js/app.js
@@ -20,14 +20,30 @@ app.controller("netCtrl", function($scope, $http, usSpinnerService, Upload, $tim
         // create a new graph using method from graph.js
         createGraph(response.data);
         $('#error').text("");
+        $('#divExport').css('visibility', 'visible');
       }else{
-        $('#error').text("You have an error!");
+        $('#error').text(" You have an error!");
+        $('#divExport').css('visibility', 'hidden');
       }
 
       //stop spinner and enable input and button
       usSpinnerService.stop('spinner-1');
       $("#txtUserAction").prop("disabled",false);
       $("#btnSubmit").prop("disabled",false);
+    });
+  });
+
+  $('#btnExport').click(function(){
+    $http({
+      method: 'POST',
+      url: 'exportGraph',
+      headers: {'Content-Type': 'application/json'},
+      data: { 'fmat': $('#selectFmat').val() }
+    }).then(function(response) {
+      if(response.data !="error"){
+        //a neat way to open up a download window
+        window.location.assign(response.data);
+      }
     });
   });
 
